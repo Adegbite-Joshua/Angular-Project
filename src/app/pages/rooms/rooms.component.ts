02732e85ea2c2441696c5d6e39d8f247a8753746
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-06-08 15:41:00 +0100

    Created the rooms page

--- src/app/pages/rooms/rooms.component.ts
+++ src/app/pages/rooms/rooms.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 import { GeneralNavbarComponent } from '../../components/navbar/general-navbar/general-navbar.component';
+import { RoomComponent } from '../../components/rooms/room/room.component';
 
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [GeneralNavbarComponent],
+  imports: [GeneralNavbarComponent, RoomComponent, FooterComponent],
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss'
 })
 export class RoomsComponent {
-
 }
